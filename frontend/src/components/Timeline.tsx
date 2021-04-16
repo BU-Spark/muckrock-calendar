14a33bc096c8a255f98c2f841b74086e18a161e4
unknown <wangkk@bu.edu>
2021-04-16 00:12:37 -0400

    TODO: CORs related error

--- frontend/src/components/Timeline.tsx
+++ frontend/src/components/Timeline.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import TimelineItem from './TimelineItem';
 import { FOIA, timelineData } from '../data';
 
+import {getFOIA} from '../service/foia'
 /**
  * Timeline Class, that build up by TimelineItem
  * @returns 
  */
+
 const Timeline = () => {
+    const _foia = getFOIA();
     const timelineList = timelineData.map(( _data: FOIA, i: any) =>  
     <div className="timeline-container">
       <TimelineItem data={timelineData[0]} /> 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './Home.css';
 import Timeline from '../components/Timeline';
 
+
+
+
 function Home() {
   return (
     <>
